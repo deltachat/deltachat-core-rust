1fd91f2da815f2f9877531093b1f8b4772613955
dignifiedquire <dignifiedquire@users.noreply.github.com>
2019-04-28 15:15:16 +0300

    build: basics for android

--- build.rs
+++ build.rs
@@ -31,11 +31,8 @@ fn main() {
     build_tools();
 
     add_search_path("/usr/local/lib");
-
-    link_dylib("sasl2");
-    link_dylib("z");
-    link_dylib("pthread");
-    link_dylib("tools");
+    add_search_path("./include/include");
+    add_search_path("./include/libs");
 
     if std::env::var("TARGET").unwrap().contains("-apple") {
         link_static("etpan");
@@ -44,7 +41,39 @@ fn main() {
         link_framework("CoreFoundation");
         link_framework("CoreServices");
         link_framework("Security");
-    } else if std::env::var("TARGET").unwrap().contains("linux") {
+
+        link_dylib("sasl2");
+        link_dylib("z");
+        link_dylib("pthread");
+        link_dylib("tools");
+    } else if std::env::var("TARGET").unwrap().contains("-android") {
+        add_search_path("./include/libs/arm64-v8a");
+        add_search_path("./include/cyrus-sasl-android-4/libs/arm64-v8a");
+        add_search_path("./include/cyrus-sasl-android-4/include");
+        add_search_path("./include/openssl-android-3/libs/arm64-v8a");
+        add_search_path("./include/openssl-android-3/include");
+        add_search_path("./include/iconv-android-1/libs/arm64-v8a");
+        add_search_path("./include/iconv-android-1/include");
+
+        // dependencies for libetpan
+        link_static("crypto");
+        link_static("sasl2");
+        link_static("iconv");
+        link_static("ssl");
+        link_dylib("z");
+
+        // libetpan iteself
+        link_static("etpan");
+
+        // local tools
+        link_static("tools");
+    } else if std::env::var("TARGET").unwrap().contains("-linux") {
         link_dylib("etpan");
+        link_dylib("sasl2");
+        link_dylib("z");
+        link_dylib("pthread");
+        link_dylib("tools");
+    } else {
+        panic!("unsupported target");
     }
 }
