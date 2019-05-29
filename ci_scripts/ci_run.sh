780426e0bffdf755d15b6d12c5e87190350eee2a
holger krekel <holger@merlinux.eu>
2019-05-29 16:05:34 +0200

    don't run doxygen anymore

--- ci_scripts/ci_run.sh
+++ ci_scripts/ci_run.sh
@@ -9,9 +9,11 @@ export BRANCH=${CIRCLE_BRANCH:-test7}
 # XXX modifies the host filesystem docs/xml and docs/html directories
 # XXX which you can then only remove with sudo as they belong to root
 
-if [ -n "$DOCS" ] ; then 
-    docker run --rm -it -v $PWD:/mnt -w /mnt/docs deltachat/doxygen doxygen
-fi
+# XXX we don't do doxygen doc generation with Rust anymore, needs to be
+# substituted with rust-docs 
+#if [ -n "$DOCS" ] ; then 
+#    docker run --rm -it -v $PWD:/mnt -w /mnt/docs deltachat/doxygen doxygen
+#fi
 
 # run everything else inside docker (TESTS, DOCS, WHEELS) 
 docker run -e BRANCH -e TESTS -e DOCS \
