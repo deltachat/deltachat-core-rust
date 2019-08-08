69e516aa51f8ec15ea5211b440a04766064b1c11
Simon Laux <mobile.info@simonlaux.de>
2019-08-08 16:18:20 +0200

    cargo fmt

--- src/dc_lot.rs
+++ src/dc_lot.rs
@@ -173,8 +173,13 @@ pub unsafe fn dc_lot_fill(
 
     let message_text = (*msg).text.as_ref().unwrap();
 
-    (*lot).text2 =
-        dc_msg_get_summarytext_by_raw((*msg).type_0, &(*(&message_text).strdup()), &mut (*msg).param, 160, context);
+    (*lot).text2 = dc_msg_get_summarytext_by_raw(
+        (*msg).type_0,
+        &(*(&message_text).strdup()),
+        &mut (*msg).param,
+        160,
+        context,
+    );
 
     (*lot).timestamp = dc_msg_get_timestamp(msg);
     (*lot).state = (*msg).state;
