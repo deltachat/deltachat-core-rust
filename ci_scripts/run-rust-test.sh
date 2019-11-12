6953386d65e94ef1337be145de109ba0645370c8
holger krekel <holger@merlinux.eu>
2019-11-12 22:04:42 +0100

    fix typo

--- ci_scripts/run-rust-test.sh
+++ ci_scripts/run-rust-test.sh
@@ -6,7 +6,7 @@ export RUST_TEST_THREADS=1
 export RUST_BACKTRACE=1
 export RUSTFLAGS='--deny warnings'
 export OPT="-j1 --target=$TARGET"
-export OPT_RELASE="--release ${OPT}"
+export OPT_RELEASE="--release ${OPT}"
 export OPT_FFI_RELEASE="--manifest-path=deltachat-ffi/Cargo.toml --release"
 
 # Select cargo command: use cross by default
