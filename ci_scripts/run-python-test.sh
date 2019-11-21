9ec405250c419f680ad791aa7e72d4dc343215bd
holger krekel <holger@merlinux.eu>
2019-11-21 12:10:14 +0100

    don't invoke py36/py35 anymore here.

--- ci_scripts/run-python-test.sh
+++ ci_scripts/run-python-test.sh
@@ -18,16 +18,15 @@ rm -rf tests/__pycache__
 rm -rf src/deltachat/__pycache__
 export PYTHONDONTWRITEBYTECODE=1
 
-# run tox. The circle-ci project env-var-setting DCC_PY_LIVECONFIG 
-# allows running of "liveconfig" tests but for speed reasons
-# we run them only for the highest python version we support
+# make sure we have proper settings to run Online tests 
+X=${DCC_PY_LIVECONFIG:?need env var to run Online tests}
 
+# run python tests (tox invokes pytest to run tests in python/tests)
 # we split out qr-tests run to minimize likelyness of flaky tests
 # (some qr tests are pretty heavy in terms of send/received
 # messages and async-imap's likely has concurrency problems, 
 # eg https://github.com/async-email/async-imap/issues/4 )
-tox -e lint,py37 -- --reruns 3 -k "not qr"
-tox -e py37 -- --reruns 5 -k "qr"
+tox -e lint,py37 -- --reruns 3 -k "not qr" tests/
+tox -e py37 -- --reruns 5 -k "qr" tests/
 unset DCC_PY_LIVECONFIG
-tox  -p4 -e lint,py35,py36,doc
 
