49fb566d1cc1018ae36fba8003c864c996bfcd46
holger krekel <holger@merlinux.eu>
2019-11-21 17:59:11 +0100

    re-enable cross, some streamlining of docs

--- ci_scripts/run-python-test.sh
+++ ci_scripts/run-python-test.sh
@@ -23,10 +23,6 @@ rm -rf src/deltachat/__pycache__
 export PYTHONDONTWRITEBYTECODE=1
 
 # run python tests (tox invokes pytest to run tests in python/tests)
-# we split out qr-tests run to minimize likelyness of flaky tests
-# (some qr tests are pretty heavy in terms of send/received
-# messages and async-imap's likely has concurrency problems, 
-# eg https://github.com/async-email/async-imap/issues/4 )
 tox -e lint,py37 
 unset DCC_PY_LIVECONFIG
 
