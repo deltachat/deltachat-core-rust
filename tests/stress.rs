d0700daa0da350f152e47b3386d6d935e4fec0f1
Alexander Krotov <ilabdsf@gmail.com>
2019-11-30 12:39:17 +0100

    Move OAuth 2 stress tests to oauth2 module

--- tests/stress.rs
+++ tests/stress.rs
@@ -234,26 +234,6 @@ fn test_dc_get_oauth2_url() {
 }
 
 #[test]
-fn test_dc_get_oauth2_addr() {
-    let ctx = create_test_context();
-    let addr = "dignifiedquire@gmail.com";
-    let code = "fail";
-    let res = dc_get_oauth2_addr(&ctx.ctx, addr, code);
-    // this should fail as it is an invalid password
-    assert_eq!(res, None);
-}
-
-#[test]
-fn test_dc_get_oauth2_token() {
-    let ctx = create_test_context();
-    let addr = "dignifiedquire@gmail.com";
-    let code = "fail";
-    let res = dc_get_oauth2_access_token(&ctx.ctx, addr, code, false);
-    // this should fail as it is an invalid password
-    assert_eq!(res, None);
-}
-
-#[test]
 fn test_stress_tests() {
     unsafe {
         let context = create_test_context();
