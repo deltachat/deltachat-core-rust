69960c2cef51bef107496363df03540b6845e77b
Alexander Krotov <ilabdsf@gmail.com>
2020-01-09 14:55:01 +0300

    Fix inverted should_do_gossip condition

--- src/mimefactory.rs
+++ src/mimefactory.rs
@@ -305,7 +305,7 @@ impl<'a, 'b> MimeFactory<'a, 'b> {
                 let chat = self.chat.as_ref().unwrap();
                 // beside key- and member-changes, force re-gossip every 48 hours
                 let gossiped_timestamp = chat.get_gossiped_timestamp(self.context);
-                if gossiped_timestamp == 0 || (gossiped_timestamp + (2 * 24 * 60 * 60)) > time() {
+                if time() > gossiped_timestamp + (2 * 24 * 60 * 60) {
                     return true;
                 }
 
