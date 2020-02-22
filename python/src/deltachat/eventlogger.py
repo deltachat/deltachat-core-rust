ec67b3975c215025320c53d6556ea9ac59794258
holger krekel <holger@merlinux.eu>
2020-02-22 17:43:00 +0100

    good bye global plugin manager ... we only do per-account object plugin_management for now

--- python/src/deltachat/eventlogger.py
+++ python/src/deltachat/eventlogger.py
@@ -2,7 +2,7 @@ import threading
 import re
 import time
 from queue import Queue, Empty
-from .hookspec import hookimpl
+from .hookspec import account_hookimpl
 
 
 class EventLogger:
@@ -18,11 +18,10 @@ class EventLogger:
         self._timeout = None
         self.init_time = time.time()
 
-    @hookimpl
-    def process_low_level_event(self, account, event_name, data1, data2):
-        if self.account == account:
-            self._log_event(event_name, data1, data2)
-            self._event_queue.put((event_name, data1, data2))
+    @account_hookimpl
+    def process_low_level_event(self, event_name, data1, data2):
+        self._log_event(event_name, data1, data2)
+        self._event_queue.put((event_name, data1, data2))
 
     def set_timeout(self, timeout):
         self._timeout = timeout
