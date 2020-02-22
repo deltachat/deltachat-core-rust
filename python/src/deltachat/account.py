e17b411b27e44da57cbdd69667d5fb6ad405b3f3
holger krekel <holger@merlinux.eu>
2020-02-22 16:54:54 +0100

    another fix, thanks @adbenitez

--- python/src/deltachat/account.py
+++ python/src/deltachat/account.py
@@ -511,7 +511,7 @@ class Account(object):
             deltachat.clear_context_callback(self._dc_context)
             del self._dc_context
             atexit.unregister(self.shutdown)
-        self.pluggy.unregister(self)
+        self.pluggy.unregister(self._evlogger)
 
     def set_location(self, latitude=0.0, longitude=0.0, accuracy=0.0):
         """set a new location. It effects all chats where we currently
