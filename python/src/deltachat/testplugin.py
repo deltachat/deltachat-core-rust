1290c191b3a2b964c58953511840269123912197
Hocuri <hocuri@gmx.de>
2020-05-23 10:42:48 +0200

    Move direct_imap to deltachat folder

--- python/src/deltachat/testplugin.py
+++ python/src/deltachat/testplugin.py
@@ -17,7 +17,7 @@ from .capi import lib
 from .events import FFIEventLogger, FFIEventTracker
 from _pytest._code import Source
 
-from direct_imap import ImapConn
+from .direct_imap import ImapConn
 
 import deltachat
 
