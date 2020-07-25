3ebb44c2c0547cb0c8fee7e59545a441e434c57d
Hocuri <hocuri@gmx.de>
2020-07-25 17:33:18 +0200

    Delete export_backup_old

--- src/dc_tools.rs
+++ src/dc_tools.rs
@@ -450,26 +450,6 @@ pub fn dc_open_file_std<P: AsRef<std::path::Path>>(
     }
 }
 
-pub(crate) async fn dc_get_next_backup_path_old(
-    folder: impl AsRef<Path>,
-    backup_time: i64,
-) -> Result<PathBuf, Error> {
-    let folder = PathBuf::from(folder.as_ref());
-    let stem = chrono::NaiveDateTime::from_timestamp(backup_time, 0)
-        .format("delta-chat-%Y-%m-%d")
-        .to_string();
-
-    // 64 backup files per day should be enough for everyone
-    for i in 0..64 {
-        let mut path = folder.clone();
-        path.push(format!("{}-{}.bak", stem, i));
-        if !path.exists().await {
-            return Ok(path);
-        }
-    }
-    bail!("could not create backup file, disk full?");
-}
-
 pub(crate) async fn dc_get_next_backup_path_new(
     folder: impl AsRef<Path>,
     backup_time: i64,
