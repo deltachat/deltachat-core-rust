aae816369637021888eaf0fa22254e5f364eea7d
holger krekel <holger@merlinux.eu>
2020-08-23 08:03:40 +0200

    try harder to let all build processes die when ssh dies

--- ci_scripts/remote_tests_python.sh
+++ ci_scripts/remote_tests_python.sh
@@ -22,8 +22,12 @@ set +x
 
 echo "--- Running $CIRCLE_JOB remotely"
 
-ssh $SSHTARGET <<_HERE
+ssh -t -t $SSHTARGET <<_HERE
     set +x -e
+
+    # make sure all processes exit when ssh dies
+    shopt -s huponexit
+
     export RUSTC_WRAPPER=\`which sccache\`
     cd $BUILDDIR
     # let's share the target dir with our last run on this branch/job-type
--- ci_scripts/remote_tests_rust.sh
+++ ci_scripts/remote_tests_rust.sh
@@ -18,8 +18,10 @@ rsync --delete --files-from=.rsynclist -az ./ "$SSHTARGET:$BUILDDIR"
 
 echo "--- Running $CIRCLE_JOB remotely"
 
-ssh $SSHTARGET <<_HERE
+ssh -t -t $SSHTARGET <<_HERE
     set +x -e
+    # make sure all processes exit when ssh dies
+    shopt -s huponexit
     export RUSTC_WRAPPER=\`which sccache\`
     cd $BUILDDIR
     # let's share the target dir with our last run on this branch/job-type
