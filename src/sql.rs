0485c55718a5f8161507e5fdb799e43c503b263a
Alexander Krotov <ilabdsf@gmail.com>
2020-09-18 19:41:41 +0300

    sql: create new accounts in one transaction
    This prevents SQLite from synchronizing to disk after each statement,
    saving time on high latency HDDs.

--- src/sql.rs
+++ src/sql.rs
@@ -713,8 +713,9 @@ async fn open(
                 "First time init: creating tables in {:?}.",
                 dbfile.as_ref(),
             );
-            sql.with_conn(move |conn| {
-                conn.execute_batch(
+            sql.with_conn(move |mut conn| {
+                let tx = conn.transaction()?;
+                tx.execute_batch(
                     r#"
 CREATE TABLE config (id INTEGER PRIMARY KEY, keyname TEXT, value TEXT);
 CREATE INDEX config_index1 ON config (keyname);
@@ -899,6 +900,7 @@ CREATE TABLE devmsglabels (
 CREATE INDEX devmsglabels_index1 ON devmsglabels (label);
 "#,
                 )?;
+                tx.commit()?;
                 Ok(())
             })
             .await?;
