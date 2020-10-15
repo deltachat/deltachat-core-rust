aa26c52813a9f148880eeecb253710d8414ca027
B. Petersen <r10s@b44t.com>
2020-10-15 22:39:27 +0200

    add prefetch config value

--- src/configure/mod.rs
+++ src/configure/mod.rs
@@ -353,11 +353,13 @@ async fn configure(ctx: &Context, param: &mut LoginParam) -> Result<()> {
     e2ee::ensure_secret_key_exists(ctx).await?;
     info!(ctx, "key generation completed");
 
-    job::add(
-        ctx,
-        job::Job::new(Action::FetchExistingMsgs, 0, Params::new(), 0),
-    )
-    .await;
+    if ctx.get_config_bool(Config::Prefetch).await {
+        job::add(
+            ctx,
+            job::Job::new(Action::FetchExistingMsgs, 0, Params::new(), 0),
+        )
+        .await;
+    }
 
     progress!(ctx, 940);
     update_device_chats_handle.await?;
