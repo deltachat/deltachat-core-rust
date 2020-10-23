63bd790894bad4695744b91099786effadcff000
B. Petersen <r10s@b44t.com>
2020-10-23 19:48:33 +0200

    add icon-group

--- src/chat.rs
+++ src/chat.rs
@@ -981,6 +981,12 @@ impl Chat {
             if !image_rel.is_empty() {
                 return Ok(Some(dc_get_abs_path(context, image_rel)));
             }
+        }
+
+        if self.typ == Chattype::Group {
+            if let Ok(image_rel) = get_group_icon(context).await {
+                return Ok(Some(dc_get_abs_path(context, image_rel)));
+            }
         } else if self.typ == Chattype::Single {
             let contacts = get_chat_contacts(context, self.id).await?;
             if let Some(contact_id) = contacts.first() {
@@ -1424,6 +1430,21 @@ pub(crate) async fn update_device_icon(context: &Context) -> Result<()> {
         contact.update_param(context).await?;
     }
     Ok(())
+}
+
+pub(crate) async fn get_group_icon(context: &Context) -> Result<String> {
+    if let Some(icon) = context.sql.get_raw_config("icon-group").await? {
+        return Ok(icon);
+    }
+
+    let icon = include_bytes!("../assets/icon-group.png");
+    let blob = BlobObject::create(context, "icon-group.png", icon).await?;
+    let icon = blob.as_name().to_string();
+    context
+        .sql
+        .set_raw_config("icon-group", Some(&icon))
+        .await?;
+    Ok(icon)
 }
 
 async fn update_special_chat_name(context: &Context, contact_id: u32, name: String) -> Result<()> {
