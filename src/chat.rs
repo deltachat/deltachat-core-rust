5bbbe4b79cea10b373917ee2f416d662e2a787ac
Alexander Krotov <ilabdsf@gmail.com>
2020-12-29 01:30:27 +0300

    Missing paren in the comment

--- src/chat.rs
+++ src/chat.rs
@@ -1255,7 +1255,7 @@ pub async fn create_by_contact_id(context: &Context, contact_id: u32) -> Result<
     let chat_id = match lookup_by_contact_id(context, contact_id).await {
         Ok((chat_id, chat_blocked)) => {
             if chat_blocked != Blocked::Not {
-                // unblock chat (typically move it from the deaddrop to view
+                // unblock chat (typically move it from the deaddrop to view)
                 chat_id.unblock(context).await;
             }
             chat_id
