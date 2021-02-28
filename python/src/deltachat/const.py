e8eeedc827755938f4e99d6642acddcd2913872e
Hocuri <hocuri@gmx.de>
2021-02-28 17:30:14 +0100

    Remove the need for manual DC_IMEX_* constants in python

--- python/src/deltachat/const.py
+++ python/src/deltachat/const.py
@@ -5,8 +5,3 @@ for name in dir(lib):
     if name.startswith("DC_"):
         globals()[name] = getattr(lib, name)
 del name
-
-DC_IMEX_EXPORT_SELF_KEYS = 1
-DC_IMEX_IMPORT_SELF_KEYS = 2
-DC_IMEX_EXPORT_BACKUP = 11
-DC_IMEX_IMPORT_BACKUP = 12
