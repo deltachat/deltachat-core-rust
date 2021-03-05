285308e32db4c6a5e748a9635a3386e18354a435
Hocuri <hocuri@gmx.de>
2021-03-05 22:16:05 +0100

    Even nicer logging: Add log_m()
    I started to implement this as I wanted to use `log_m()` in
    https://github.com/deltachat/deltachat-core-rust/pull/2283 but then
    decided to make an extra PR

--- src/log.rs
+++ src/log.rs
@@ -1,5 +1,4 @@
 //! # Logging
-
 use crate::context::Context;
 
 #[macro_export]
@@ -68,10 +67,41 @@ pub trait LogExt<T> {
     /// - You won't get any warnings about unused results but can still use the value if you need it
     /// - This prevents the same warning from being printed to the log multiple times
     ///
+    /// Example: This:
+    /// ```
+    /// if let Err(e) = do_something() {
+    ///     warn!(context, "{:#}", e);
+    /// }
+    /// ```
+    /// can be replaced with:
+    /// ```
+    /// do_something().log(context);
+    /// ```
+    ///
     /// Thanks to the [track_caller](https://blog.rust-lang.org/2020/08/27/Rust-1.46.0.html#track_caller)
     /// feature, the location of the caller is printed to the log, just like with the warn!() macro.
+    ///
+    /// Unfortunately, the track_caller feature does not work on async functions (as of Rust 1.50).
+    /// This means that we can't make our logs even better by adding `#[track_caller]` to our helper
+    /// functions.  
+    /// See https://github.com/rust-lang/rust/issues/78840 for progress on this.
     #[track_caller]
     fn log(self, context: &Context) -> Option<T>;
+
+    /// Like `log()`, but you can pass an extra string message.
+    ///
+    /// Example: This:
+    /// ```
+    /// if let Err(e) = do_something() {
+    ///     warn!(context, "Something went wrong: {:#}", e);
+    /// }
+    /// ```
+    /// can be replaced with:
+    /// ```
+    /// do_something().log_m(context, "Something went wrong");
+    /// ```
+    #[track_caller]
+    fn log_m(self, context: &Context, msg: &str) -> Option<T>;
 }
 
 impl<T> LogExt<T> for anyhow::Result<T> {
@@ -88,7 +118,29 @@ impl<T> LogExt<T> for anyhow::Result<T> {
                     e = e
                 );
                 // We can't use the warn!() macro here as the file!() and line!() macros
-                // don't work well with #[track_caller]
+                // don't work with #[track_caller]
+                emit_event!(context, crate::EventType::Warning(full));
+                None
+            }
+            Ok(v) => Some(v),
+        }
+    }
+
+    #[track_caller]
+    fn log_m(self, context: &Context, msg: &str) -> Option<T> {
+        match self {
+            Err(e) => {
+                let location = std::panic::Location::caller();
+                // We are using Anyhow's .context() and to show the inner error, too, we need the {:#}:
+                let full = format!(
+                    "{file}:{line}: {msg}: {e:#}",
+                    file = location.file(),
+                    line = location.line(),
+                    msg = msg,
+                    e = e
+                );
+                // We can't use the warn!() macro here as the file!() and line!() macros
+                // don't work with #[track_caller]
                 emit_event!(context, crate::EventType::Warning(full));
                 None
             }
@@ -96,3 +148,27 @@ impl<T> LogExt<T> for anyhow::Result<T> {
         }
     }
 }
+
+// #[track_caller]
+// fn do_something_with_sql(context: &Context) {
+//     // context
+//     //     .sql
+//     //     .table_exists("config")
+//     //     .await
+//     goes_wrong().log_m(context, "Can't do something");
+// }
+
+// fn goes_wrong() -> anyhow::Result<()> {
+//     Err(anyhow::format_err!("went wrong"))
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     #[async_std::test]
+//     async fn test_log() {
+//         let t = crate::test_utils::TestContext::new_alice().await;
+//         t.sql.close().await;
+//         do_something_with_sql(&t);
+//     }
+// }
