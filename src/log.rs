b7ade1171a706b3145eb897a540e36508d6dbe45
Hocuri <hocuri@gmx.de>
2021-03-10 10:13:13 +0100

    Rename and add `log_err()` like flub proposed

--- src/log.rs
+++ src/log.rs
@@ -1,5 +1,6 @@
 //! # Logging
 use crate::context::Context;
+use anyhow::Context as _;
 
 #[macro_export]
 macro_rules! info {
@@ -63,25 +64,6 @@ macro_rules! emit_event {
 pub trait LogExt<T> {
     /// Emits a warning if the receiver contains an Err value.
     ///
-    /// If you want to add context to the log, use `log_m()`.
-    ///
-    /// Returns an [`Option<T>`] with the `Ok(_)` value, if any:
-    /// - You won't get any warnings about unused results but can still use the value if you need it
-    /// - This prevents the same warning from being printed to the log multiple times
-    ///
-    // Sidenote: If 'javascript' below is removed or replaced with 'ignore', then the CI doc-tests
-    // fail (apparently, ignoring doesn't work)
-    /// Example: This:
-    /// ```javascript
-    /// if let Err(e) = do_something() {
-    ///     warn!(context, "{:#}", e);
-    /// }
-    /// ```
-    /// can be replaced with:
-    /// ```javascript
-    /// do_something().log(context);
-    /// ```
-    ///
     /// Thanks to the [track_caller](https://blog.rust-lang.org/2020/08/27/Rust-1.46.0.html#track_caller)
     /// feature, the location of the caller is printed to the log, just like with the warn!() macro.
     ///
@@ -91,67 +73,79 @@ pub trait LogExt<T> {
     /// like warn!(), since the file!() and line!() macros don't work with track_caller)  
     /// See https://github.com/rust-lang/rust/issues/78840 for progress on this.
     #[track_caller]
-    fn log(self, context: &Context) -> Option<T>;
+    fn log_err(self, context: &Context) -> Self;
 
-    /// Like `log()`, but you can pass an extra message that is prepended in the log.
+    /// Emits a warning if the receiver contains an Err value.
     ///
-    // Sidenote: If 'javascript' below is removed or replaced with 'ignore', then the CI doc-tests
-    // fail (apparently, ignoring doesn't work)
-    /// Example: This:
-    /// ```javascript
+    /// Unlike `log_err()`, returns an [`Option<T>`] instead of Self.
+    ///
+    /// Example:
+    /// ```text
+    /// if let Err(e) = do_something() {
+    ///     warn!(context, "{:#}", e);
+    /// }
+    /// ```
+    /// is equivalent to:
+    /// ```text
+    /// do_something().ok_or_log(context);
+    /// ```
+    /// and is also equivalent to:
+    /// ```text
+    /// do_something().log_err(context).ok();
+    /// ```
+    ///
+    /// For a note on the `track_caller` feature, see the doc comment on `log_err()`.
+    #[track_caller]
+    fn ok_or_log(self, context: &Context) -> Option<T>;
+
+    /// Like `ok_or_log()`, but you can pass an extra message that is prepended in the log.
+    ///
+    /// Example:
+    /// ```text
     /// if let Err(e) = do_something() {
     ///     warn!(context, "Something went wrong: {:#}", e);
     /// }
     /// ```
-    /// can be replaced with:
-    /// ```javascript
-    /// do_something().log_m(context, "Something went wrong");
+    /// is equivalent to:
+    /// ```text
+    /// do_something().ok_or_log_msg(context, "Something went wrong");
     /// ```
+    /// and is also equivalent to:
+    /// ```text
+    /// do_something().context("Something went wrong").ok_or_log(context);
+    /// ```
+    ///
+    /// For a note on the `track_caller` feature, see the doc comment on `log_err()`.
     #[track_caller]
-    fn log_m(self, context: &Context, msg: &str) -> Option<T>;
+    fn ok_or_log_msg(self, context: &Context, msg: &'static str) -> Option<T>;
 }
 
 impl<T> LogExt<T> for anyhow::Result<T> {
     #[track_caller]
-    fn log(self, context: &Context) -> Option<T> {
-        match self {
-            Err(e) => {
-                let location = std::panic::Location::caller();
-                // We are using Anyhow's .context() and to show the inner error, too, we need the {:#}:
-                let full = format!(
-                    "{file}:{line}: {e:#}",
-                    file = location.file(),
-                    line = location.line(),
-                    e = e
-                );
-                // We can't use the warn!() macro here as the file!() and line!() macros
-                // don't work with #[track_caller]
-                emit_event!(context, crate::EventType::Warning(full));
-                None
-            }
-            Ok(v) => Some(v),
-        }
+    fn log_err(self, context: &Context) -> Self {
+        self.map_err(|e| {
+            let location = std::panic::Location::caller();
+            // We are using Anyhow's .context() and to show the inner error, too, we need the {:#}:
+            let full = format!(
+                "{file}:{line}: {e:#}",
+                file = location.file(),
+                line = location.line(),
+                e = e
+            );
+            // We can't use the warn!() macro here as the file!() and line!() macros
+            // don't work with #[track_caller]
+            emit_event!(context, crate::EventType::Warning(full));
+            e
+        })
     }
 
     #[track_caller]
-    fn log_m(self, context: &Context, msg: &str) -> Option<T> {
-        match self {
-            Err(e) => {
-                let location = std::panic::Location::caller();
-                // We are using Anyhow's .context() and to show the inner error, too, we need the {:#}:
-                let full = format!(
-                    "{file}:{line}: {msg}: {e:#}",
-                    file = location.file(),
-                    line = location.line(),
-                    msg = msg,
-                    e = e
-                );
-                // We can't use the warn!() macro here as the file!() and line!() macros
-                // don't work with #[track_caller]
-                emit_event!(context, crate::EventType::Warning(full));
-                None
-            }
-            Ok(v) => Some(v),
-        }
+    fn ok_or_log(self, context: &Context) -> Option<T> {
+        self.log_err(context).ok()
+    }
+
+    #[track_caller]
+    fn ok_or_log_msg(self, context: &Context, msg: &'static str) -> Option<T> {
+        self.context(msg).log_err(context).ok()
     }
 }
