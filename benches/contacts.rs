24bb3e3ce9cc11101f8247a6efb2bb3a906e95e1
link2xt <link2xt@testrun.org>
2021-04-17 18:40:40 +0300

    Benchmark reading contact list

--- benches/contacts.rs
+++ benches/contacts.rs
@@ -4,7 +4,7 @@ use deltachat::contact::Contact;
 use deltachat::context::Context;
 use tempfile::tempdir;
 
-async fn address_book_benchmark(n: u32) {
+async fn address_book_benchmark(n: u32, read_count: u32) {
     let dir = tempdir().unwrap();
     let dbfile = dir.path().join("db.sqlite");
     let id = 100;
@@ -18,11 +18,20 @@ async fn address_book_benchmark(n: u32) {
         .join("");
 
     Contact::add_address_book(&context, book).await.unwrap();
+
+    let query: Option<&str> = None;
+    for _ in 0..read_count {
+        Contact::get_all(&context, 0, query).await.unwrap();
+    }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("create 500 contacts", |b| {
-        b.iter(|| block_on(async { address_book_benchmark(black_box(500)).await }))
+        b.iter(|| block_on(async { address_book_benchmark(black_box(500), black_box(0)).await }))
+    });
+
+    c.bench_function("create 100 contacts and read it 1000 times", |b| {
+        b.iter(|| block_on(async { address_book_benchmark(black_box(100), black_box(1000)).await }))
     });
 }
 
