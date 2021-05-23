7e3bfd38f0140d8c3b9723016bba3fed133c0921
link2xt <link2xt@testrun.org>
2021-05-23 18:20:54 +0300

    scripts: use rust 1.52.1 for docker-coredeps-arm64
    Building aarch64 wheels under qemu takes a long time.
    rust 1.52.1 includes fix for cargo that may result in
    compilation being stuck: https://github.com/rust-lang/cargo/pull/9201

--- scripts/docker-coredeps-arm64/deps/build_rust.sh
+++ scripts/docker-coredeps-arm64/deps/build_rust.sh
@@ -8,9 +8,9 @@ set -e -x
 #
 # Avoid using rustup here as it depends on reading /proc/self/exe and
 # has problems running under QEMU.
-curl "https://static.rust-lang.org/dist/rust-1.50.0-$(uname -m)-unknown-linux-gnu.tar.gz" | tar xz
-cd "rust-1.50.0-$(uname -m)-unknown-linux-gnu"
+curl "https://static.rust-lang.org/dist/rust-1.52.1-$(uname -m)-unknown-linux-gnu.tar.gz" | tar xz
+cd "rust-1.52.1-$(uname -m)-unknown-linux-gnu"
 ./install.sh --prefix=/usr --components=rustc,cargo,"rust-std-$(uname -m)-unknown-linux-gnu"
 rustc --version
 cd ..
-rm -fr "rust-1.50.0-$(uname -m)-unknown-linux-gnu"
+rm -fr "rust-1.52.1-$(uname -m)-unknown-linux-gnu"
