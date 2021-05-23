2a83147d90e3fede4fe77daf59968ed663143748
link2xt <link2xt@testrun.org>
2021-05-23 18:40:40 +0300

    tox: fix "test command found but not installed in testenv" warning
    Determine architecture from python instead of using sh and uname

--- python/tests/auditwheels.py
+++ python/tests/auditwheels.py
@@ -1,13 +1,14 @@
 
 import os
-import sys
+import platform
 import subprocess
+import sys
 
 
 if __name__ == "__main__":
-    assert len(sys.argv) == 3
+    assert len(sys.argv) == 2
     workspacedir = sys.argv[1]
-    arch = sys.argv[2]
+    arch = platform.machine()
     for relpath in os.listdir(workspacedir):
         if relpath.startswith("deltachat"):
             p = os.path.join(workspacedir, relpath)
