09f5b015bb13946d3718c935ad1a6098618513b3
link2xt <link2xt@testrun.org>
2021-05-23 11:39:23 +0300

    python: support running auditwheels for aarch64

--- python/tests/auditwheels.py
+++ python/tests/auditwheels.py
@@ -5,11 +5,12 @@ import subprocess
 
 
 if __name__ == "__main__":
-    assert len(sys.argv) == 2
+    assert len(sys.argv) == 3
     workspacedir = sys.argv[1]
+    arch = sys.argv[2]
     for relpath in os.listdir(workspacedir):
         if relpath.startswith("deltachat"):
             p = os.path.join(workspacedir, relpath)
             subprocess.check_call(
                 ["auditwheel", "repair", p, "-w", workspacedir,
-                 "--plat", "manylinux2014_x86_64"])
+                 "--plat", "manylinux2014_" + arch])
