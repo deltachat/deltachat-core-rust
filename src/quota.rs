1f7053f140dbf76fa2926458e67bbdebe1402d6e
Simon Laux <mobile.info@simonlaux.de>
2021-06-27 03:01:07 +0200

    make quota_usage_report_job result important (to send notification)

--- src/quota.rs
+++ src/quota.rs
@@ -39,7 +39,7 @@ pub(crate) async fn quota_usage_report_job(context: &Context, imap: &mut Imap) -
         // build and send report message
         let mut msg = Message::new(Viewtype::Text);
         msg.text = Some(generate_report_message(&unique_quota_roots, context).await?);
-        add_device_msg(context, None, Some(&mut msg)).await?;
+        add_device_msg_with_importance(context, None, Some(&mut msg), true).await?;
     } else {
         warn!(
             context,
