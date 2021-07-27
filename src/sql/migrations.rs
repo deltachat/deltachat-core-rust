b83bdfc696f97e813ef601a0269c262b6e483839
B. Petersen <r10s@b44t.com>
2021-07-27 23:41:10 +0200

    move requests but the last one to "Archived Chats"
    this way, the app looks familiar after the contact request upgrade.
    the subselect was copied from the old get_last_deaddrop_fresh_msg()
    (which was removed by the contact request upgrade #2514)

--- src/sql/migrations.rs
+++ src/sql/migrations.rs
@@ -468,6 +468,22 @@ paramsv![]
         recode_avatar = true;
         sql.set_db_version(77).await?;
     }
+    if dbversion < 78 {
+        // move requests but the last one to "Archived Chats";
+        // this way, the app looks familiar after the contact request upgrade.
+        info!(context, "[migration] v78");
+        sql.execute_migration(
+            "UPDATE chats
+                SET archived=1
+              WHERE blocked=2
+                AND id!=COALESCE((SELECT chat_id FROM msgs m
+                        LEFT JOIN chats c ON c.id=m.chat_id
+                            WHERE m.state=10 AND m.hidden=0 AND c.blocked=2
+                         ORDER BY m.timestamp DESC, m.id DESC), 0);",
+            78,
+        )
+        .await?;
+    }
 
     Ok((
         recalc_fingerprints,
