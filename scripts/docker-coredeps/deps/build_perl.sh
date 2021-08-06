0276938975c2b90bd4e4fb2643b8ccab37f76f93
link2xt <link2xt@testrun.org>
2021-08-07 00:00:00 +0300

    ci: update perl
    Old miniperl segfaults on the latest manylinux image

--- scripts/docker-coredeps/deps/build_perl.sh
+++ scripts/docker-coredeps/deps/build_perl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERL_VERSION=5.30.0
-# PERL_SHA256=7e929f64d4cb0e9d1159d4a59fc89394e27fa1f7004d0836ca0d514685406ea8
+PERL_VERSION=5.34.0
+# PERL_SHA256=551efc818b968b05216024fb0b727ef2ad4c100f8cb6b43fab615fa78ae5be9a
 curl -O https://www.cpan.org/src/5.0/perl-${PERL_VERSION}.tar.gz
 # echo "${PERL_SHA256}  perl-${PERL_VERSION}.tar.gz" | sha256sum -c -
 tar -xzf perl-${PERL_VERSION}.tar.gz
