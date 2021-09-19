72dcf7093a270fed44780a5e880a65bacf092d9c
B. Petersen <r10s@b44t.com>
2021-09-19 13:50:09 +0200

    add missing stock strings for connectivity and quota

--- deltachat-ffi/deltachat.h
+++ deltachat-ffi/deltachat.h
@@ -5900,6 +5900,70 @@ void dc_event_unref(dc_event_t* event);
 /// `%1$s` will be replaced by human-readable date and time.
 #define DC_STR_DOWNLOAD_AVAILABILITY      100
 
+/// "Inbox"
+///
+/// Used as a headline in the connectivity view.
+#define DC_STR_INBOX                      103
+
+/// "Outbox"
+///
+/// Used as a headline in the connectivity view.
+#define DC_STR_OUTBOX                     104
+
+/// "Storage on %1$s"
+///
+/// Used as a headline in the connectivity view.
+///
+/// `%1$s` will be replaced by the domain of the configured email-address.
+#define DC_STR_STORAGE_ON_DOMAIN          105
+
+/// "One moment…"
+///
+/// Used in the connectivity view when some information are not yet there.
+#define DC_STR_ONE_MOMENT                 106
+
+/// "Connected"
+///
+/// Used as status in the connectivity view.
+#define DC_STR_CONNECTED                  107
+
+/// "Connecting…"
+///
+/// Used as status in the connectivity view.
+#define DC_STR_CONNTECTING                108
+
+/// "Updating…"
+///
+/// Used as status in the connectivity view.
+#define DC_STR_UPDATING                   109
+
+/// "Sending…"
+///
+/// Used as status in the connectivity view.
+#define DC_STR_SENDING                    110
+
+/// "Your last message was sent successfully."
+///
+/// Used as status in the connectivity view.
+#define DC_STR_LAST_MSG_SENT_SUCCESSFULLY 111
+
+/// "Error: %1$s"
+///
+/// Used as status in the connectivity view.
+///
+/// `%1$s` will be replaced by a possibly more detailed, typically english, error description.
+#define DC_STR_ERROR                      112
+
+/// "Quota not supported by your provider."
+///
+/// Used in the connectivity view.
+#define DC_STR_QUOTA_NOT_SUPPORTED        113
+
+/// "Messages"
+///
+/// Used as a subtitle in quota context; can be plural always.
+#define DC_STR_MESSAGES                   114
+
 /**
  * @}
  */
