ddefd2cf09a406768c6581397d5a64ac693b018f
link2xt <link2xt@testrun.org>
2021-11-21 13:22:49 +0000

    python: add cutil.from_optional_dc_charpointer()
    `cutil.from_dc_charpointer()` is guaranteed to return `str`, while
    `cutil.from_optional_dc_charpointer()` may return `None` if C function
    returns `NULL`.

--- python/src/deltachat/contact.py
+++ python/src/deltachat/contact.py
@@ -1,10 +1,11 @@
 """ Contact object. """
 
 from . import props
-from .cutil import from_dc_charpointer
+from .cutil import from_dc_charpointer, from_optional_dc_charpointer
 from .capi import lib, ffi
 from .chat import Chat
 from . import const
+from typing import Optional
 
 
 class Contact(object):
@@ -35,12 +36,12 @@ class Contact(object):
         )
 
     @props.with_doc
-    def addr(self):
+    def addr(self) -> str:
         """ normalized e-mail address for this account. """
         return from_dc_charpointer(lib.dc_contact_get_addr(self._dc_contact))
 
     @props.with_doc
-    def name(self):
+    def name(self) -> str:
         """ display name for this contact. """
         return from_dc_charpointer(lib.dc_contact_get_display_name(self._dc_contact))
 
@@ -67,15 +68,13 @@ class Contact(object):
         """ Return True if the contact is verified. """
         return lib.dc_contact_is_verified(self._dc_contact)
 
-    def get_profile_image(self):
+    def get_profile_image(self) -> Optional[str]:
         """Get contact profile image.
 
         :returns: path to profile image, None if no profile image exists.
         """
         dc_res = lib.dc_contact_get_profile_image(self._dc_contact)
-        if dc_res == ffi.NULL:
-            return None
-        return from_dc_charpointer(dc_res)
+        return from_optional_dc_charpointer(dc_res)
 
     @property
     def status(self):
