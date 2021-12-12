92fab0174ef04c025dcd6fb64e9b0aa997d5b205
B. Petersen <r10s@b44t.com>
2021-12-12 21:52:33 +0100

    wrap payloads to json-object on the wire

--- src/mimefactory.rs
+++ src/mimefactory.rs
@@ -1159,7 +1159,9 @@ impl<'a> MimeFactory<'a> {
             let json = self.msg.param.get(Param::Arg).unwrap_or_default();
             parts.push(context.build_status_update_part(json).await);
         } else if self.msg.viewtype == Viewtype::W30 {
-            let json = context.get_w30_status_updates(self.msg.id, None).await?;
+            let json = context
+                .get_w30_status_updates_with_format(self.msg.id, None, true)
+                .await?;
             if json != "[]" {
                 parts.push(context.build_status_update_part(&json).await);
             }
