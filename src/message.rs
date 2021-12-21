5cef124d138159001cd6a6535ad8db3120cc5e11
Sebastian Klähn <scoreplayer2000@gmail.com>
2021-12-21 15:07:03 +0100

    fix "no hop info" not shown

--- src/message.rs
+++ src/message.rs
@@ -1177,10 +1177,8 @@ pub async fn get_msg_info(context: &Context, msg_id: MsgId) -> Result<String> {
         .query_get_value("SELECT hop_info FROM msgs WHERE id=?;", paramsv![msg_id])
         .await?;
 
-    if hop_info.is_some() {
-        ret.push_str(&hop_info.unwrap_or_else(|| "No Hop info".to_owned()));
-    }
-
+    ret.push_str(&hop_info.unwrap_or_else(|| "No Hop info".to_owned()));
+    
     Ok(ret)
 }
 
