1aca087a2c899de8ad75c0a0a4b38c964000b23f
Hocuri <hocuri@gmx.de>
2022-04-08 12:00:00 +0200

    Remove now-unused `ephemeral_task` JoinHandle
    The JoinHandle is now inside the Scheduler.

--- src/context.rs
+++ src/context.rs
@@ -56,7 +56,6 @@ pub struct InnerContext {
     pub(crate) events: Events,
 
     pub(crate) scheduler: RwLock<Scheduler>,
-    pub(crate) ephemeral_task: RwLock<Option<task::JoinHandle<()>>>,
 
     /// Recently loaded quota information, if any.
     /// Set to `None` if quota was never tried to load.
@@ -176,7 +175,6 @@ impl Context {
             translated_stockstrings: RwLock::new(HashMap::new()),
             events: Events::default(),
             scheduler: RwLock::new(Scheduler::Stopped),
-            ephemeral_task: RwLock::new(None),
             quota: RwLock::new(None),
             creation_time: std::time::SystemTime::now(),
             last_full_folder_scan: Mutex::new(None),
@@ -641,10 +639,6 @@ impl InnerContext {
                 let lock = &mut *self.scheduler.write().await;
                 lock.stop(token).await;
             }
-        }
-
-        if let Some(ephemeral_task) = self.ephemeral_task.write().await.take() {
-            ephemeral_task.cancel().await;
         }
     }
 }
