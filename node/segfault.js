d2d35fe26bbef065c4f9ab9d71542fbe60c8340c
Simon Laux <mobile.info@simonlaux.de>
2022-06-13 19:01:54 +0200

    call a jsonrpc function in segfault example

--- node/segfault.js
+++ node/segfault.js
@@ -1,11 +1,22 @@
-const {default:dc} = require("./dist")
+const { default: dc } = require('./dist')
 
-const ac = new dc("testdtrdtrh")
+const ac = new dc('test123456')
 
 ac.startJSONRPCHandler(console.log)
 
-setTimeout(()=>{
-ac.close() // This segfaults -> TODO Findout why?
+console.log(
+  ac.jsonRPCRequest(
+    JSON.stringify({
+      jsonrpc: '2.0',
+      method: 'get_all_account_ids',
+      params: [],
+      id: 2,
+    })
+  )
+)
 
-console.log("still living")
+setTimeout(() => {
+  ac.close() // This segfaults -> TODO Findout why?
+
+  console.log('still living')
 }, 1000)
