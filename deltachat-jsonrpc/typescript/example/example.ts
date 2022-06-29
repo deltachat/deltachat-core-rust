329f498651d7ed7ddeb42a89308c6d403ee9bd48
Franz Heinzmann (Frando) <frando@unbiskant.org>
2022-06-29 17:53:45 +0200

    improve docs, fix example

--- deltachat-jsonrpc/typescript/example/example.ts
+++ deltachat-jsonrpc/typescript/example/example.ts
@@ -1,7 +1,5 @@
-import { RawClient } from "../src/lib";
-import { WebsocketTransport, Request } from "yerpc";
+import { Deltachat, DeltachatEvent } from "../deltachat.js";
 
-type DeltaEvent = { id: string; contextId: number; field1: any; field2: any };
 var SELECTED_ACCOUNT = 0;
 
 window.addEventListener("DOMContentLoaded", (_event) => {
@@ -9,6 +7,7 @@ window.addEventListener("DOMContentLoaded", (_event) => {
     SELECTED_ACCOUNT = Number(id);
     window.dispatchEvent(new Event("account-changed"));
   };
+  console.log('launch run script...')
   run().catch((err) => console.error("run failed", err));
 });
 
@@ -17,31 +16,24 @@ async function run() {
   const $side = document.getElementById("side")!;
   const $head = document.getElementById("header")!;
 
-  const transport = new WebsocketTransport("ws://localhost:20808/ws");
-  const client = new RawClient(transport);
-  transport.on("error", (err) => {
-    console.error("WebSocket transport error", err)
-  });
+  const client = new Deltachat('ws://localhost:20808/ws')
 
-  ;(window as any).client = client;
+  ;(window as any).client = client.rpc;
 
-  transport.on("request", (request: Request) => {
-    const method = request.method;
-    if (method === "event") {
-      const params = request.params! as DeltaEvent;
-      onIncomingEvent(params, params.id);
-    }
-  });
+  client.on("ALL", event => {
+    onIncomingEvent(event)
+  })
 
   window.addEventListener("account-changed", async (_event: Event) => {
-    await client.selectAccount(SELECTED_ACCOUNT);
     listChatsForSelectedAccount();
   });
 
   await Promise.all([loadAccountsInHeader(), listChatsForSelectedAccount()]);
 
   async function loadAccountsInHeader() {
-    const accounts = await client.getAllAccounts();
+    console.log('load accounts')
+    const accounts = await client.rpc.getAllAccounts();
+    console.log('accounts loaded', accounts)
     for (const account of accounts) {
       if (account.type === "Configured") {
         write(
@@ -63,31 +55,30 @@ async function run() {
 
   async function listChatsForSelectedAccount() {
     clear($main);
-    const selectedAccount = await client.getSelectedAccountId();
-    if (!selectedAccount) return write($main, "No account selected");
-    const info = await client.getAccountInfo(selectedAccount);
+    const selectedAccount = SELECTED_ACCOUNT
+    const info = await client.rpc.getAccountInfo(selectedAccount);
     if (info.type !== "Configured") {
       return write($main, "Account is not configured");
     }
     write($main, `<h2>${info.addr!}</h2>`);
-    const chats = await client.getChatlistEntries(
+    const chats = await client.rpc.getChatlistEntries(
       selectedAccount,
       0,
       null,
       null
     );
     for (const [chatId, _messageId] of chats) {
-      const chat = await client.chatlistGetFullChatById(
+      const chat = await client.rpc.chatlistGetFullChatById(
         selectedAccount,
         chatId
       );
       write($main, `<h3>${chat.name}</h3>`);
-      const messageIds = await client.messageListGetMessageIds(
+      const messageIds = await client.rpc.messageListGetMessageIds(
         selectedAccount,
         chatId,
         0
       );
-      const messages = await client.messageGetMessages(
+      const messages = await client.rpc.messageGetMessages(
         selectedAccount,
         messageIds
       );
@@ -97,12 +88,12 @@ async function run() {
     }
   }
 
-  function onIncomingEvent(event: DeltaEvent, name: string) {
+  function onIncomingEvent(event: DeltachatEvent) {
     write(
       $side,
       `
         <p class="message">
-          [<strong>${name}</strong> on account ${event.contextId}]<br>
+          [<strong>${event.id}</strong> on account ${event.contextId}]<br>
           <em>f1:</em> ${JSON.stringify(event.field1)}<br>
           <em>f2:</em> ${JSON.stringify(event.field2)}
         </p>`
