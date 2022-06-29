e3fa42fe88b6d244a61ede8ca01c6d73d24a9f58
Franz Heinzmann (Frando) <frando@unbiskant.org>
2022-06-29 22:25:20 +0200

    don't wait for IO on webserver start

--- deltachat-jsonrpc/src/webserver.rs
+++ deltachat-jsonrpc/src/webserver.rs
@@ -13,6 +13,7 @@ const DEFAULT_PORT: u16 = 20808;
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), std::io::Error> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
     let path = std::env::var("DC_ACCOUNTS_PATH").unwrap_or_else(|_| "./accounts".to_string());
     let port = std::env::var("DC_PORT")
         .map(|port| port.parse::<u16>().expect("DC_PORT must be a number"))
@@ -20,11 +21,16 @@ async fn main() -> Result<(), std::io::Error> {
     log::info!("Starting with accounts directory `{path}`.");
     let accounts = Accounts::new(PathBuf::from(&path)).await.unwrap();
     let state = CommandApi::new(accounts);
+
     let app = Router::new()
         .route("/ws", get(handler))
         .layer(Extension(state.clone()));
+
+    tokio::spawn(async move {
+        state.accounts.read().await.start_io().await;
+    });
+
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
-    state.accounts.read().await.start_io().await;
     log::info!("JSON-RPC WebSocket server listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
