77c5c54223eb55f986b1490e87083ebdfebc67ed
link2xt <link2xt@testrun.org>
2022-07-11 23:20:57 +0000

    package_wheels.py: get version from setuptools_scm without using setup.py
    `python -m setuptools_scm` is a documented (in setuptools_scm README)
    way to get version and it does not print any other output
    that has to be filtered out.

--- python/tests/package_wheels.py
+++ python/tests/package_wheels.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     # pip wheel will build in an isolated tmp dir that does not have git
     # history so setuptools_scm can not automatically determine a
     # version there. So pass in the version through an env var.
-    version = subprocess.check_output(["python", "setup.py", "--version"]).strip().split(b"\n")[-1]
+    version = subprocess.check_output(["python", "-m", "setuptools_scm"])
     os.environ["SETUPTOOLS_SCM_PRETEND_VERSION"] = version.decode("ascii")
-    subprocess.check_call(("pip wheel . -w %s" % wheelhousedir).split())
+    subprocess.check_call(["pip", "wheel", ".", "-w", wheelhousedir])
