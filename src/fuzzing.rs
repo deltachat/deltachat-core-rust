7082f9f882c0516431abd6fb4e8d969b80ce736e
link2xt <link2xt@testrun.org>
2022-12-28 19:01:48 +0000

    Fix fuzzing module warnings

--- src/fuzzing.rs
+++ src/fuzzing.rs
@@ -1,8 +1,12 @@
+//! # Fuzzing module.
+//!
+//! This module exposes private APIs for fuzzing.
+
 /// Fuzzing target for simplify().
 ///
 /// Calls simplify() and panics if simplify() panics.
 /// Does not return any vaule to avoid exposing internal crate types.
 #[cfg(fuzzing)]
-pub fn simplify(mut input: String, is_chat_message: bool) {
+pub fn simplify(input: String, is_chat_message: bool) {
     crate::simplify::simplify(input, is_chat_message);
 }
