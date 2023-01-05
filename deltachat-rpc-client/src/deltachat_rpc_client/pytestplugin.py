f4c674fa98c93e2ec5727e51337aba0f9d317356
link2xt <link2xt@testrun.org>
2023-01-05 22:59:12 +0000

    python: set reasonable timeouts for account requests
    `requests` library does not have a timeout at all by default.

--- deltachat-rpc-client/src/deltachat_rpc_client/pytestplugin.py
+++ deltachat-rpc-client/src/deltachat_rpc_client/pytestplugin.py
@@ -12,8 +12,11 @@ from .rpc import Rpc
 async def get_temp_credentials() -> dict:
     url = os.getenv("DCC_NEW_TMP_EMAIL")
     assert url, "Failed to get online account, DCC_NEW_TMP_EMAIL is not set"
+
+    # Replace default 5 minute timeout with a 1 minute timeout.
+    timeout = aiohttp.ClientTimeout(total=60)
     async with aiohttp.ClientSession() as session:
-        async with session.post(url) as response:
+        async with session.post(url, timeout=timeout) as response:
             return json.loads(await response.text())
 
 
