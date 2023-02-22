cff4a9dce3f9d882587772d938177f865b94d0b0
link2xt <link2xt@testrun.org>
2023-02-22 19:50:42 +0000

    ci: cross-compile deltachat-rpc-server for aarch64 Linux

---
+++ scripts/aarch64-unknown-linux-musl.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -x
+set -e
+
+# Download Zig
+rm -fr zig-linux-x86_64-0.10.1 zig-linux-x86_64-0.10.1.tar.xz
+wget https://ziglang.org/download/0.10.1/zig-linux-x86_64-0.10.1.tar.xz
+tar xf zig-linux-x86_64-0.10.1.tar.xz
+export PATH="$PATH:$PWD/zig-linux-x86_64-0.10.1"
+
+cargo install cargo-zigbuild
+
+rustup target add aarch64-unknown-linux-musl
+
+cargo zigbuild --release --target aarch64-unknown-linux-musl -p deltachat-rpc-server --features vendored
