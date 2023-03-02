239793fe2a95b40fcccd6ac330051c23f6ba4b94
Floris Bruynooghe <flub@devork.be>
2023-03-02 11:56:11 +0100

    ci: Make sure clippy script check everything
    Also disable missing_docs_in_private_items as this is like 300+ errors
    currently.

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Run clippy for all Rust code in the project.
-cargo clippy --workspace --tests --examples --benches -- -D warnings
+cargo clippy --workspace --all-targets -- -D warnings
