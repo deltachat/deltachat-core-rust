f84773b179aec0e64cd8a2d7c858068726900f20
link2xt <link2xt@testrun.org>
2023-05-09 15:58:27 +0000

    Move reaction sorting to the core and reuse it for display implementation

--- deltachat-jsonrpc/src/api/types/reactions.rs
+++ deltachat-jsonrpc/src/api/types/reactions.rs
@@ -11,13 +11,12 @@ pub struct JSONRPCReactions {
     /// Map from a contact to it's reaction to message.
     reactions_by_contact: BTreeMap<u32, Vec<String>>,
     /// Unique reactions and their count, sorted in descending order.
-    reactions: Vec<(String, u32)>,
+    reactions: Vec<(String, usize)>,
 }
 
 impl From<Reactions> for JSONRPCReactions {
     fn from(reactions: Reactions) -> Self {
         let mut reactions_by_contact: BTreeMap<u32, Vec<String>> = BTreeMap::new();
-        let mut unique_reactions: BTreeMap<String, u32> = BTreeMap::new();
 
         for contact_id in reactions.contacts() {
             let reaction = reactions.get(contact_id);
@@ -30,23 +29,11 @@ impl From<Reactions> for JSONRPCReactions {
                 .map(|emoji| emoji.to_owned())
                 .collect();
             reactions_by_contact.insert(contact_id.to_u32(), emojis.clone());
-            for emoji in emojis {
-                if let Some(x) = unique_reactions.get_mut(&emoji) {
-                    *x += 1;
-                } else {
-                    unique_reactions.insert(emoji, 1);
-                }
-            }
         }
 
-        let mut unique_reactions: Vec<(String, u32)> = unique_reactions
-            .into_iter()
-            .map(|(emoji, count)| (emoji, count))
-            .collect();
-        unique_reactions.sort_by(|a, b| a.1.cmp(&b.1));
         JSONRPCReactions {
             reactions_by_contact,
-            reactions: unique_reactions,
+            reactions: reactions.emoji_sorted_by_frequency(),
         }
     }
 }
