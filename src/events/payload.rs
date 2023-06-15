6442e13a1a25354529b0c34e9300ec3d66d9abc6
Simon Laux <mobile.info@simonlaux.de>
2023-06-15 15:07:19 +0200

    add missing docs

--- src/events/payload.rs
+++ src/events/payload.rs
@@ -285,5 +285,8 @@ pub enum EventType {
 
     /// Inform UI that a single chat list item changed and needs to be rerendered
     /// If `chat_id` is set to None, then all currently visible chats need to be rerendered, and all not-visible items need to be cleared from cache if the UI has a cache.
-    UIChatListItemChanged { chat_id: Option<ChatId> },
+    UIChatListItemChanged { 
+        /// ID of the changed chat
+        chat_id: Option<ChatId>
+    },
 }
