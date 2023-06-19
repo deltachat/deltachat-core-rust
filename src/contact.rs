7483aa532b106fe2e49af66b894f2500a687105d
Simon Laux <mobile.info@simonlaux.de>
2023-06-19 18:26:51 +0200

    fix formatting

--- src/contact.rs
+++ src/contact.rs
@@ -805,7 +805,9 @@ impl Contact {
             // update the chats the contact that changed their name is part of
             // (treefit): could make sense to only update chats where the last message is from the contact, but the db query for that is more expensive
             for chat_id in Contact::get_chats_with_contact(context, &contact_id).await? {
-                context.emit_event(EventType::UIChatListItemChanged { chat_id: Some(chat_id) });
+                context.emit_event(EventType::UIChatListItemChanged {
+                    chat_id: Some(chat_id),
+                });
             }
         }
 
@@ -1611,6 +1613,7 @@ pub(crate) async fn set_profile_image(
     if changed {
         contact.update_param(context).await?;
         context.emit_event(EventType::ContactsChanged(Some(contact_id)));
+        // TODO update DM chat
     }
     Ok(())
 }
@@ -1817,6 +1820,7 @@ impl RecentlySeenLoop {
                         // Timeout, notify about contact.
                         if let Some(contact_id) = contact_id {
                             context.emit_event(EventType::ContactsChanged(Some(*contact_id)));
+                            // TODO update DM chat
                             unseen_queue.pop();
                         }
                     }
@@ -1846,6 +1850,7 @@ impl RecentlySeenLoop {
                 // Event is already in the past.
                 if let Some(contact_id) = contact_id {
                     context.emit_event(EventType::ContactsChanged(Some(*contact_id)));
+                    // TODO update DM chat
                 }
                 unseen_queue.pop();
             }
