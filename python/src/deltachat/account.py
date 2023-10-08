352367e77a720ea5b92940dfa0a95226bcc29fc2
missytake <missytake@systemli.org>
2023-10-08 08:58:32 +0200

    fix(python): don't automatically set the displayname to 'bot' when setting log level

--- python/src/deltachat/account.py
+++ python/src/deltachat/account.py
@@ -617,9 +617,7 @@ class Account:
     # meta API for start/stop and event based processing
     #
 
-    def run_account(self, addr=None, password=None, account_plugins=None, show_ffi=False):
-        from .events import FFIEventLogger
-
+    def run_account(self, addr=None, password=None, account_plugins=None, show_ffi=False, displayname=None):
         """get the account running, configure it if necessary. add plugins if provided.
 
         :param addr: the email address of the account
@@ -627,8 +625,9 @@ class Account:
         :param account_plugins: a list of plugins to add
         :param show_ffi: show low level ffi events
         """
+        from .events import FFIEventLogger
+
         if show_ffi:
-            self.set_config("displayname", "bot")
             log = FFIEventLogger(self)
             self.add_account_plugin(log)
 
@@ -644,6 +643,8 @@ class Account:
             configtracker = self.configure()
             configtracker.wait_finish()
 
+        if displayname:
+            self.set_config("displayname", displayname)
         # start IO threads and configure if necessary
         self.start_io()
 
