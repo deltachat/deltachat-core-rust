b4fe9e3eece6ee4acb7158f292fed450906e8ed4
link2xt <link2xt@testrun.org>
2023-10-22 08:46:37 +0000

    build: use Zig via `ziglang` PyPI package
    This avoids cluttering workdir with unpacked tarball
    and will work automatically once PEP-723 is implemented.

--- scripts/zig-rpc-server.sh
+++ scripts/zig-rpc-server.sh
@@ -10,14 +10,6 @@ unset RUSTFLAGS
 # Pin Rust version to avoid uncontrolled changes in the compiler and linker flags.
 export RUSTUP_TOOLCHAIN=1.72.0
 
-ZIG_VERSION=0.11.0
-
-# Download Zig
-rm -fr "$ZIG_VERSION" "zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-wget "https://ziglang.org/builds/zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-tar xf "zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-export PATH="$PWD/zig-linux-x86_64-$ZIG_VERSION:$PATH"
-
 rustup target add i686-unknown-linux-musl
 CC="$PWD/scripts/zig-cc" \
 TARGET_CC="$PWD/scripts/zig-cc" \
