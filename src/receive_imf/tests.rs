a6ac50cd5ee7760d6cb235f9f692f60eb15dc2a7
iequidoo <dgreshilov@gmail.com>
2023-12-06 02:18:26 -0300

    api!: Remove msg_id (last message ID) from Chatlist::get_summary2() (#3071)
    `Chatlist::get_summary2()` is used in the jsonrpc API. CFFI uses `Chatlist::get_summary()` which
    logic is preserved (it uses message ids cached in the `Chatlist`).
    
    The motivation for this change is that jsonrpc API uses `get_last_message_for_chat()` returning the
    message id + `Chatlist::get_summary2()` taking the message id when building a chatlist, but if the
    message is ephemeral, the id can expire and building the chatlist would fail. The solution is to
    return a summary for the last message (if any) in the chat from `Chatlist::get_summary2()` so the
    call to `get_last_message_for_chat()` goes away (and two SQL queries are just merged into one) and
    overall the API is easier to use.
    
    Also this change extends `struct Summary` with the message viewtype (so an extra call to
    `Message::load_from_db()` goes away) and the message id in case if it's needed for any purposes
    beyond building the chatlist.

--- src/receive_imf/tests.rs
+++ src/receive_imf/tests.rs
@@ -320,6 +320,7 @@ async fn test_no_from() {
     let context = &t;
 
     let chats = Chatlist::try_load(&t, 0, None, None).await.unwrap();
+    assert!(chats.get_chat_id(0).is_err());
     assert!(chats.get_msg_id(0).is_err());
 
     let received = receive_imf(
@@ -362,6 +363,7 @@ async fn test_no_message_id_header() {
     let t = TestContext::new_alice().await;
 
     let chats = Chatlist::try_load(&t, 0, None, None).await.unwrap();
+    assert!(chats.get_chat_id(0).is_err());
     assert!(chats.get_msg_id(0).is_err());
 
     let received = receive_imf(
@@ -464,8 +466,9 @@ async fn test_escaped_recipients() {
     assert_eq!(contact.get_display_name(), "h2");
 
     let chats = Chatlist::try_load(&t, 0, None, None).await.unwrap();
-    let msg = Message::load_from_db(&t, chats.get_msg_id(0).unwrap().unwrap())
+    let msg = Message::load_from_db_last_for_chat(&t, chats.get_chat_id(0).unwrap())
         .await
+        .unwrap()
         .unwrap();
     assert_eq!(msg.is_dc_message, MessengerMessage::Yes);
     assert_eq!(msg.text, "hello");
@@ -652,7 +655,7 @@ async fn test_parse_ndn(
     .unwrap();
 
     let chats = Chatlist::try_load(&t, 0, None, None).await.unwrap();
-    let msg_id = chats.get_msg_id(0).unwrap().unwrap();
+    let chat_id = chats.get_chat_id(0).unwrap();
 
     // Check that the ndn would be downloaded:
     let headers = mailparse::parse_mail(raw_ndn).unwrap().headers;
@@ -663,7 +666,10 @@ async fn test_parse_ndn(
     );
 
     receive_imf(&t, raw_ndn, false).await.unwrap();
-    let msg = Message::load_from_db(&t, msg_id).await.unwrap();
+    let msg = Message::load_from_db_last_for_chat(&t, chat_id)
+        .await
+        .unwrap()
+        .unwrap();
 
     assert_eq!(
         msg.state,
@@ -733,8 +739,11 @@ async fn load_imf_email(context: &Context, imf_raw: &[u8]) -> Message {
         .unwrap();
     receive_imf(context, imf_raw, false).await.unwrap();
     let chats = Chatlist::try_load(context, 0, None, None).await.unwrap();
-    let msg_id = chats.get_msg_id(0).unwrap().unwrap();
-    Message::load_from_db(context, msg_id).await.unwrap()
+    let chat_id = chats.get_chat_id(0).unwrap();
+    Message::load_from_db_last_for_chat(context, chat_id)
+        .await
+        .unwrap()
+        .unwrap()
 }
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
