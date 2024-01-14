1895f4c556b416cbb73caa9f8cbfa999a1fd507d
link2xt <link2xt@testrun.org>
2024-01-14 00:32:55 +0000

    chore(release): prepare for 1.133.0

--- scripts/set_core_version.py
+++ scripts/set_core_version.py
@@ -102,7 +102,7 @@ def main():
                 found = True
         if not found:
             raise SystemExit(
-                f"{changelog_name} contains no entry for version: {newversion}"
+                f"CHANGELOG.md contains no entry for version: {newversion}"
             )
 
     for toml_filename in toml_list:
