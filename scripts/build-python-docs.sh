73492ca4bccaa2eb7057a37acad32f9c0de54017
link2xt <link2xt@testrun.org>
2024-02-13 11:20:11 +0000

    chore(scripts): do not install deltachat-rpc-client twice

--- scripts/build-python-docs.sh
+++ scripts/build-python-docs.sh
@@ -9,5 +9,4 @@ python3 -m venv venv
 venv/bin/pip install ./python
 venv/bin/pip install ./deltachat-rpc-client
 venv/bin/pip install sphinx breathe sphinx_rtd_theme
-venv/bin/pip install ./deltachat-rpc-client
 venv/bin/sphinx-build -b html -a python/doc/ dist/html
