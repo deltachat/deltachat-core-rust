2eeaa311b44170f0e0fb54ba1d1ab07f9e740ce1
link2xt <link2xt@testrun.org>
2024-04-25 17:04:59 +0000

    Box MagicEndpoint building

--- src/peer_channels.rs
+++ src/peer_channels.rs
@@ -31,31 +31,33 @@ impl Context {
             return Ok(());
         }
 
-        let endpoint = MagicEndpoint::builder()
-            .secret_key(secret_key)
-            .alpns(vec![GOSSIP_ALPN.to_vec()])
-            .relay_mode(
-                /* self.metadata
-                .read()
-                .await
-                .as_ref()
-                .map(|conf| {
-                    let url = conf
-                        .iroh_relay
-                        .as_deref()
-                        .unwrap_or("https://iroh.testrun.org:4443");
-                    let url = RelayUrl::from(Url::parse(url)?);
-                    Ok::<_, url::ParseError>(RelayMode::Custom(RelayMap::from_url(url)))
-                })
-                .transpose()?
-                // This should later be RelayMode::Disable as soon as chatmail servers have relay servers
-                .unwrap_or(RelayMode::Default), */
-                RelayMode::Custom(RelayMap::from_url(RelayUrl::from(
-                    Url::parse("https://iroh.testrun.org:4443").unwrap(),
-                ))),
-            )
-            .bind(0)
-            .await?;
+        let endpoint = Box::pin(
+            MagicEndpoint::builder()
+                .secret_key(secret_key)
+                .alpns(vec![GOSSIP_ALPN.to_vec()])
+                .relay_mode(
+                    /* self.metadata
+                    .read()
+                    .await
+                    .as_ref()
+                    .map(|conf| {
+                        let url = conf
+                            .iroh_relay
+                            .as_deref()
+                            .unwrap_or("https://iroh.testrun.org:4443");
+                        let url = RelayUrl::from(Url::parse(url)?);
+                        Ok::<_, url::ParseError>(RelayMode::Custom(RelayMap::from_url(url)))
+                    })
+                    .transpose()?
+                    // This should later be RelayMode::Disable as soon as chatmail servers have relay servers
+                    .unwrap_or(RelayMode::Default), */
+                    RelayMode::Custom(RelayMap::from_url(RelayUrl::from(
+                        Url::parse("https://iroh.testrun.org:4443").unwrap(),
+                    ))),
+                )
+                .bind(0),
+        )
+        .await?;
 
         // create gossip
         let my_addr = endpoint.my_addr().await?;
