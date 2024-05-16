a0f0a8e02154cf4bc92969aa7f9b0be14ad3195b
link2xt <link2xt@testrun.org>
2024-05-16 14:16:21 +0000

    build: add repository URL to deltachat-rpc-server packages

--- deltachat-rpc-server/npm-package/scripts/src/make_package.py
+++ deltachat-rpc-server/npm-package/scripts/src/make_package.py
@@ -13,14 +13,21 @@ def write_package_json(platform_path, rust_target, my_binary_name):
     tomlfile = open("../../Cargo.toml", 'rb')
     version = tomllib.load(tomlfile)['package']['version']
 
-    package_json = dict({
-        "name": "@deltachat/stdio-rpc-server-" + convert_os_to_npm_os(os) + "-" + convert_cpu_arch_to_npm_cpu_arch(cpu_arch),
+    package_json = {
+        "name": "@deltachat/stdio-rpc-server-"
+        + convert_os_to_npm_os(os)
+        + "-"
+        + convert_cpu_arch_to_npm_cpu_arch(cpu_arch),
         "version": version,
         "os": [convert_os_to_npm_os(os)],
         "cpu": [convert_cpu_arch_to_npm_cpu_arch(cpu_arch)],
         "main": my_binary_name,
-        "license": "MPL-2.0"
-    })
+        "license": "MPL-2.0",
+        "repository": {
+            "type": "git",
+            "url": "https://github.com/deltachat/deltachat-core-rust.git",
+        },
+    }
 
     file = open(platform_path + "/package.json", 'w')
     file.write(json.dumps(package_json, indent=4))
