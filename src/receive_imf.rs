46d829f9fc7b68e58581fbecd51a407d71ea87f5
link2xt <link2xt@testrun.org>
2024-05-16 19:36:39 +0000

    fix: do not mark the message as seen if it has location.kml

--- src/receive_imf.rs
+++ src/receive_imf.rs
@@ -1014,7 +1014,6 @@ async fn add_parts(
             || fetching_existing_messages
             || is_mdn
             || is_reaction
-            || is_location_kml
             || chat_id_blocked == Blocked::Yes
         {
             MessageState::InSeen
