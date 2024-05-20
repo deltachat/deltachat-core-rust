dd77d32446e7c4c6669c21258ebb48ba1f9c535e
Simon Laux <Simon-Laux@users.noreply.github.com>
2024-05-20 12:47:36 +0200

    fix: log/print exit error of deltachat-rpc-server (#5601)
    see also #5599 (this logs that error atleast, but does not fix it yet)
    
    ---------
    
    Co-authored-by: iequidoo <117991069+iequidoo@users.noreply.github.com>

--- deltachat-rpc-server/src/main.rs
+++ deltachat-rpc-server/src/main.rs
@@ -29,6 +29,9 @@ async fn main() {
     // "For technical reasons, stdin is implemented by using an ordinary blocking read on a separate
     // thread, and it is impossible to cancel that read. This can make shutdown of the runtime hang
     // until the user presses enter."
+    if let Err(error) = &r {
+        log::error!("Fatal error: {error:#}.")
+    }
     std::process::exit(if r.is_ok() { 0 } else { 1 });
 }
 
