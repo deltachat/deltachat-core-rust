3b91815240a09ec30a84b0ccdc1e77e57be3f4cb
iequidoo <dgreshilov@gmail.com>
2024-06-15 14:07:51 -0300

    test(python): Set delete_server_after=1 ("delete immediately") for bots (#4976)
    Test bots are run with `fetch_existing_msgs` set, so messages must be deleted immediately not to be
    processed again after a bot redeployment.

--- python/src/deltachat/testplugin.py
+++ python/src/deltachat/testplugin.py
@@ -552,8 +552,9 @@ class ACFactory:
 
         bot_cfg = self.get_next_liveconfig()
         # The bot process is run asynchronously, so some messages can arrive before the bot is fully
-        # initialised.
+        # initialised. Also delete messages immediately to not process them again after restart.
         bot_cfg["fetch_existing_msgs"] = "1"
+        bot_cfg["delete_server_after"] = "1"
         bot_ac = self.prepare_account_from_liveconfig(bot_cfg)
 
         # Forget ac as it will be opened by the bot subprocess
