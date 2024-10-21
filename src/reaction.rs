07405ecd727f80814c16c36b17941ff4389c7813
B. Petersen <r10s@b44t.com>
2024-10-21 20:55:53 +0200

    add get_matching_opt() to test generically for the absence of events

--- src/reaction.rs
+++ src/reaction.rs
@@ -602,16 +602,11 @@ Here's my footer -- bob@example.net"
         Ok(())
     }
 
-    fn has_incoming_reactions_event(t: &TestContext) -> bool {
-        loop {
-            if let Ok(event) = t.evtracker.try_recv() {
-                if let EventType::IncomingReaction { .. } = event.typ {
-                    return true;
-                }
-            } else {
-                return false;
-            }
-        }
+    async fn has_incoming_reactions_event(t: &TestContext) -> bool {
+        t.evtracker
+            .get_matching_opt(t, |evt| matches!(evt, EventType::IncomingReaction { .. }))
+            .await
+            .is_some()
     }
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
@@ -644,7 +639,7 @@ Here's my footer -- bob@example.net"
 
         send_reaction(&bob, bob_msg.id, "👍").await.unwrap();
         expect_reactions_changed_event(&bob, bob_msg.chat_id, bob_msg.id, ContactId::SELF).await?;
-        assert!(!has_incoming_reactions_event(&bob));
+        assert!(!has_incoming_reactions_event(&bob).await);
         assert_eq!(get_chat_msgs(&bob, bob_msg.chat_id).await?.len(), 2);
 
         let bob_reaction_msg = bob.pop_sent_msg().await;
@@ -703,7 +698,7 @@ Here's my footer -- bob@example.net"
         send_reaction(&bob, bob_msg1.id, "👍").await?;
         let bob_send_reaction = bob.pop_sent_msg().await;
         alice.recv_msg_trash(&bob_send_reaction).await;
-        assert!(has_incoming_reactions_event(&alice));
+        assert!(has_incoming_reactions_event(&alice).await);
 
         let chatlist = Chatlist::try_load(&bob, 0, None, None).await?;
         let summary = chatlist.get_summary(&bob, 0, None).await?;
@@ -719,7 +714,7 @@ Here's my footer -- bob@example.net"
         send_reaction(&alice, alice_msg1.sender_msg_id, "🍿").await?;
         let alice_send_reaction = alice.pop_sent_msg().await;
         bob.recv_msg_opt(&alice_send_reaction).await;
-        assert!(!has_incoming_reactions_event(&bob));
+        assert!(!has_incoming_reactions_event(&bob).await);
 
         assert_summary(&alice, "You reacted 🍿 to \"Party?\"").await;
         assert_summary(&bob, "ALICE reacted 🍿 to \"Party?\"").await;
