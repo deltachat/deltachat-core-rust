fd4d5c98c6d2ad22cb9c1a7f2bf1143a293c369d
link2xt <link2xt@testrun.org>
2024-11-05 17:53:50 +0000

    feat: do not copy Chat-Version header into unprotected part

--- src/mimefactory.rs
+++ src/mimefactory.rs
@@ -800,7 +800,6 @@ impl MimeFactory {
                     | "in-reply-to"
                     | "references"
                     | "auto-submitted"
-                    | "chat-version"
                     | "autocrypt-setup-message" => {
                         unprotected_headers.push(header);
                     }
