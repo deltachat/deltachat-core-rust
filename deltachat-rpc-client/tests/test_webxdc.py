1859ab2473243e43220bc3a9d40b75c1420bebd0
B. Petersen <r10s@b44t.com>
2024-11-20 20:58:45 +0100

    fix python test

--- deltachat-rpc-client/tests/test_webxdc.py
+++ deltachat-rpc-client/tests/test_webxdc.py
@@ -24,6 +24,7 @@ def test_webxdc(acfactory) -> None:
         "name": "Chess Board",
         "sourceCodeUrl": None,
         "summary": None,
+        "selfAddr": webxdc_info.selfAddr
     }
 
     status_updates = message.get_webxdc_status_updates()
