c4d78e5d109b22c6eb32b00e47e2a931af20180f
link2xt <link2xt@testrun.org>
2025-02-15 02:06:27 +0000

    build: remove deprecated node module

--- node/lib/binding.ts
+++
@@ -1,9 +0,0 @@
-import { join } from 'path'
-
-/**
- * bindings are not typed yet.
- * if the available function names are required they can be found inside of `../src/module.c`
- */
-export const bindings: any = require('node-gyp-build')(join(__dirname, '../'))
-
-export default bindings
