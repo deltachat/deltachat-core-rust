f92606b49c215eed19f4fc07a2a3d29d6843b4c9
link2xt <link2xt@testrun.org>
2025-03-09 23:43:15 +0000

    remove quorum rule test

--- python/tests/test_1_online.py
+++ python/tests/test_1_online.py
@@ -1109,61 +1109,6 @@ def test_dont_show_emails(acfactory, lp):
     assert len(msg.chat.get_messages()) == 3
 
 
-def test_prefer_encrypt(acfactory, lp):
-    """Test quorum rule for encryption preference in 1:1 and group chat."""
-    ac1 = acfactory.new_online_configuring_account(fix_is_chatmail=True)
-    ac2 = acfactory.new_online_configuring_account(fix_is_chatmail=True)
-    ac3 = acfactory.new_online_configuring_account(fix_is_chatmail=True)
-    acfactory.bring_accounts_online()
-    ac1.set_config("e2ee_enabled", "0")
-    ac2.set_config("e2ee_enabled", "1")
-    ac3.set_config("e2ee_enabled", "0")
-
-    # Make sure we do not send a copy to ourselves. This is to
-    # test that we count own preference even when we are not in
-    # the recipient list.
-    ac1.set_config("bcc_self", "0")
-    ac2.set_config("bcc_self", "0")
-    ac3.set_config("bcc_self", "0")
-
-    acfactory.introduce_each_other([ac1, ac2, ac3])
-
-    lp.sec("ac1: sending message to ac2")
-    chat1 = ac1.create_chat(ac2)
-    msg1 = chat1.send_text("message1")
-    assert not msg1.is_encrypted()
-    ac2._evtracker.wait_next_incoming_message()
-
-    lp.sec("ac2: sending message to ac1")
-    chat2 = ac2.create_chat(ac1)
-    msg2 = chat2.send_text("message2")
-    # Own preference is `Mutual` and we have the peer's key.
-    assert msg2.is_encrypted()
-    ac1._evtracker.wait_next_incoming_message()
-
-    lp.sec("ac1: sending message to group chat with ac2 and ac3")
-    group = ac1.create_group_chat("hello")
-    group.add_contact(ac2)
-    group.add_contact(ac3)
-    msg3 = group.send_text("message3")
-    assert not msg3.is_encrypted()
-    ac2._evtracker.wait_next_incoming_message()
-    ac3._evtracker.wait_next_incoming_message()
-
-    lp.sec("ac3: start preferring encryption and inform ac1")
-    ac3.set_config("e2ee_enabled", "1")
-    chat3 = ac3.create_chat(ac1)
-    msg4 = chat3.send_text("message4")
-    # Own preference is `Mutual` and we have the peer's key.
-    assert msg4.is_encrypted()
-    ac1._evtracker.wait_next_incoming_message()
-
-    lp.sec("ac1: sending another message to group chat with ac2 and ac3")
-    msg5 = group.send_text("message5")
-    # Majority prefers encryption now
-    assert msg5.is_encrypted()
-
-
 def test_bot(acfactory, lp):
     """Test that bot messages can be identified as such"""
     ac1, ac2 = acfactory.get_online_accounts(2)
