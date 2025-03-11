0ef788e79ae6e990d7dd1eea6908b779fbdf9bab
link2xt <link2xt@testrun.org>
2025-03-11 21:53:15 +0000

    build: build Android wheels for PyPI

--- flake.nix
+++ flake.nix
@@ -309,10 +309,41 @@
             LD = "${targetCc}";
           };
 
-        mkAndroidPackages = arch: {
-          "deltachat-rpc-server-${arch}-android" = mkAndroidRustPackage arch "deltachat-rpc-server";
-          "deltachat-repl-${arch}-android" = mkAndroidRustPackage arch "deltachat-repl";
-        };
+        mkAndroidPackages = arch:
+          let
+            rpc-server = mkAndroidRustPackage arch "deltachat-rpc-server";
+          in
+          {
+            "deltachat-rpc-server-${arch}-android" = rpc-server;
+            "deltachat-repl-${arch}-android" = mkAndroidRustPackage arch "deltachat-repl";
+            "deltachat-rpc-server-${arch}-android-wheel" =
+              pkgs.stdenv.mkDerivation {
+                pname = "deltachat-rpc-server-${arch}-android-wheel";
+                version = manifest.version;
+                src = nix-filter.lib {
+                  root = ./.;
+                  include = [
+                    "scripts/wheel-rpc-server.py"
+                    "deltachat-rpc-server/README.md"
+                    "LICENSE"
+                    "Cargo.toml"
+                  ];
+                };
+                nativeBuildInputs = [
+                  pkgs.python3
+                  pkgs.python3Packages.wheel
+                ];
+                buildInputs = [
+                  rpc-server
+                ];
+                buildPhase = ''
+                  mkdir tmp
+                  cp ${rpc-server}/bin/deltachat-rpc-server tmp/deltachat-rpc-server
+                  python3 scripts/wheel-rpc-server.py ${arch}-android tmp/deltachat-rpc-server
+                '';
+                installPhase = ''mkdir -p $out; cp -av deltachat_rpc_server-*.whl $out'';
+              };
+          };
 
         mkRustPackages = arch:
           let
