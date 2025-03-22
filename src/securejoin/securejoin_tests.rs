f1de01db01496b64034c3e315f4fbfce85acd799
link2xt <link2xt@testrun.org>
2025-03-22 01:33:29 +0000

    fix test_unknown_sender

--- src/securejoin/securejoin_tests.rs
+++ src/securejoin/securejoin_tests.rs
@@ -711,8 +711,8 @@ async fn test_unknown_sender() -> Result<()> {
 
     let sent = bob.send_text(bob_chat_id, "Hi hi!").await;
 
-    let alice_bob_contact_id = Contact::create(&alice, "Bob", "bob@example.net").await?;
-    remove_contact_from_chat(&alice, alice_chat_id, alice_bob_contact_id).await?;
+    let alice_bob_contact = alice.add_or_lookup_pgp_contact(&bob).await;
+    remove_contact_from_chat(&alice, alice_chat_id, alice_bob_contact.id).await?;
     alice.pop_sent_msg().await;
 
     // The message from Bob is delivered late, Bob is already removed.
